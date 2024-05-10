a7436a74e6e824c79b9e98ca2ce5a7d0002e59bd
Rodrigo Girão Serrão <5621605+rodrigogiraoserrao@users.noreply.github.com>
2024-05-10 15:12:31 +0100

    Auto sync commit

---
+++ _sync.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+from time import sleep
+
+from git import Repo
+
+
+repo = Repo(Path(__file__).parent)
+
+while True:
+    repo.index.add("*")
+    repo.index.commit("Auto sync commit")
+    sleep(60)
